e528335cbe20edb00a8ba08a4314cd2c5fbf0541
Simpanoi-95 <sheillahsimpanoi@gmail.com>
2020-07-28 08:41:53 +0300

    PictureBook4

---
+++ app/src/main/java/com/example/picturebook/PictureBook4.kt
@@ -0,0 +1,11 @@
+package com.example.picturebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PictureBook4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture_book4)
+    }
+}
