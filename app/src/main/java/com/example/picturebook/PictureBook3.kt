f133313312d53f277c69403904887fe11f065a41
Simpanoi-95 <sheillahsimpanoi@gmail.com>
2020-08-13 10:20:37 +0300

    set

--- app/src/main/java/com/example/picturebook/PictureBook3.kt
+++ app/src/main/java/com/example/picturebook/PictureBook3.kt
@@ -1,11 +1,20 @@
 package com.example.picturebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class PictureBook3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture_book3)
+
+        btnPrevious.setOnClickListener{
+            startActivity(Intent(baseContext,MainActivity::class.java))
+        }
+        btnNext.setOnClickListener{
+            startActivity(Intent(baseContext,MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/picturebook/PictureBook5.kt
+++ app/src/main/java/com/example/picturebook/PictureBook5.kt
@@ -1,11 +1,20 @@
 package com.example.picturebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class PictureBook5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture_book5)
+
+        btnPrevious.setOnClickListener{
+            startActivity(Intent(baseContext,MainActivity::class.java))
+        }
+        btnNext.setOnClickListener{
+            startActivity(Intent(baseContext,MainActivity::class.java))
+        }
     }
 }
